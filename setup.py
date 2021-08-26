93aab6af2875d0fab7698a0db67b822792c3738f
Shen Zhou <shenz34206@hotmail.com>
2021-08-26 16:01:33 +0800

    Add to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name             = "hfh",
-    version          = "0.2",
-    author           = "szsdk",
-    scripts=["hfh", "bm"],
-    install_requires = ["click", "IPython"]
+    name="hfh",
+    version="0.2",
+    author="szsdk",
+    scripts=["hfh", "bm", "mysd"],
+    install_requires=["click", "IPython"],
 )
